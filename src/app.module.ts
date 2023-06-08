c7184e6b76235a1231833bd813a165c33105e4c9
ThaeN <thaenpong@gmail.com>
2023-06-08 10:04:27 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,11 @@ import { Job } from './jobs/entities/job.entity';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
-    /* host: '159.223.89.150', */
-    host: 'localhost',
+    host: '159.223.89.150',
+    /* host: 'localhost', */
     port: 3306,
     username: 'root',
-    password: '',
+    password: 'Admin1989',
     database: 'services',
     entities: [Asset, Category, Job],
     synchronize: true,
